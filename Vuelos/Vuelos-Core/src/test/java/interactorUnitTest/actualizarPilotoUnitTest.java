32758c968288e497a33d917ebd4dfac2b7968d01
Javier Ruitti <jruitti@undec.edu.ar>
2019-10-15 17:11:58 -0300

    [Vuelos-Core] Caso de uso Actualizar Piloto con test funcionando

---
+++ Vuelos/Vuelos-Core/src/test/java/interactorUnitTest/actualizarPilotoUnitTest.java
@@ -0,0 +1,46 @@
+package interactorUnitTest;
+
+import Mockito.MockitoExtension;
+import excepciones.PilotoExisteException;
+import excepciones.PilotoIncompletoException;
+import excepciones.TipoPilotoIncompletoException;
+import interactor.ActualizarPilotoUseCase;
+import modelo.Piloto;
+import modelo.TipoPiloto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import repositorio.IActualizarPilotoRepo;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class actualizarPilotoUnitTest {
+
+    @Mock
+    IActualizarPilotoRepo actualizarPilotoRepo;
+
+
+    @Test
+    public void actualizarPiloto_NoHayConflictoPilotoExistente_GuardaCorrectamente() throws TipoPilotoIncompletoException, PilotoIncompletoException, PilotoExisteException {
+        //Piloto pilotoModificar=Piloto.factoryPiloto(1,"Perez", "Juan","000111222", TipoPiloto.factoryTipoPiloto(1, "Comandante"));
+        Piloto pilotoDatosNuevos=Piloto.factoryPiloto(1,"Lopez", "Jorge","000111222", TipoPiloto.factoryTipoPiloto(1, "Comandante"));
+        when(actualizarPilotoRepo.findByDNI("000111222")).thenReturn(null);
+        when(actualizarPilotoRepo.guardar(pilotoDatosNuevos)).thenReturn(true);
+
+        ActualizarPilotoUseCase actualizarPilotoUseCase = new ActualizarPilotoUseCase(actualizarPilotoRepo);
+        boolean resultado = actualizarPilotoUseCase.actualizarPiloto(pilotoDatosNuevos);
+        Assertions.assertTrue(resultado);
+    }
+
+    @Test
+    public void actualizarPiloto_ConflictoConPiotoExistente_PilotoExisteException() throws TipoPilotoIncompletoException, PilotoIncompletoException {
+        //Piloto pilotoModificar=Piloto.factoryPiloto(1,"Perez", "Juan","12345678", TipoPiloto.factoryTipoPiloto(1, "Comandante"));
+        Piloto pilotoDatosNuevos=Piloto.factoryPiloto(1,"Lopez", "Jorge","12345678", TipoPiloto.factoryTipoPiloto(1, "Comandante"));
+        when(actualizarPilotoRepo.findByDNI("12345678")).thenReturn(Piloto.factoryPiloto(2,"Paez", "Carlos","12345678", TipoPiloto.factoryTipoPiloto(1, "Comandante")));
+        ActualizarPilotoUseCase actualizarPilotoUseCase= new ActualizarPilotoUseCase(actualizarPilotoRepo);
+        Assertions.assertThrows(PilotoExisteException.class, () -> actualizarPilotoUseCase.actualizarPiloto(pilotoDatosNuevos));
+
+    }
+}
